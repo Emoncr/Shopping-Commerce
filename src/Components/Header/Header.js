97b5cdcd3e8369477f0d3fc2fabb5b0f23ffc99b
Emoncr <biplobemon 75945@gmail.com>
2023-02-05 16:59:17 +0600

    starting

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import  logo  from "../../images/Logo.svg";
+const Header = () => {
+  return (
+    <div>
+        <img src={logo} alt="logo" />
+    </div>
+  )
+}
+
+export default Header
